ce21b5c32870a8394e336fcb467e5ad54fc02530
Thibault LEFAY <thibault.lefay.pro@gmail.com>
2025-06-10 00:32:11 +0200

    headerfooteraccueil

--- FitChallenge/app/Http/Controllers/AccueilController.php
+++ FitChallenge/app/Http/Controllers/AccueilController.php
@@ -1,15 +1,28 @@
 <?php
 
-namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+namespace App\Http\Controllers;
 use Inertia\Inertia;
+
+use App\Models\Defi;
+use App\Models\Programme;
+use App\Models\ParticipationDefi;
 
 class AccueilController extends Controller
 {
-        public function accueil()
+    public function index()
     {
-        return Inertia::render('Accueil'); 
+        $defis = Defi::latest()->take(5)->get();
+        $programmes = Programme::latest()->take(5)->get();
+        $challenges = ParticipationDefi::with('defi') // si besoin du nom du défi
+                            ->latest()
+                            ->take(5)
+                            ->get();
+
+        return Inertia::render('Accueil', [
+            'defis' => $defis,
+            'programmes' => $programmes,
+            'challenges' => $challenges,
+        ]);
     }
 }
